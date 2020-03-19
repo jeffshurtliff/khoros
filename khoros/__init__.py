cf3ae5faafc864b541335da28f3a6ba782fc076d
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2020-03-18 18:54:15 -0600

    Added the version.warn_when_not_latest() function call in the primary module

--- khoros/__init__.py
+++ khoros/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-:Package:        khoros
-:Synopsis:       This is the ``__init__`` module for the khoros package
-:Usage:          ``from khoros import Khoros``
-:Example:        ``khoros = Khoros(community_url='https://community.example.com', helper='path/to/helper_file.yml')
-:Created By:     Jeff Shurtliff
-:Last Modified:  Jeff Shurtliff
-:Modified Date:  29 Feb 2020
+:Package:           khoros
+:Synopsis:          This is the ``__init__`` module for the khoros package
+:Usage:             ``from khoros import Khoros``
+:Example:           ``khoros = Khoros(community_url='https://community.example.com', helper='path/to/helper_file.yml')``
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     18 Mar 2020
 """
 
 from . import errors
@@ -17,3 +17,6 @@ __all__ = ['core', 'auth', 'liql', 'errors']
 
 # Define the package version by pulling from the khoros.utils.version module
 __version__ = version.get_full_version()
+
+# Display a warning if the running version is not the latest stable version found on PyPI
+version.warn_when_not_latest()
