4d724ee5837f8b90e4807574c0bb3e078f2c9a28
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2020-03-26 13:56:10 -0600

    Added docstring to khoros.utils.tests

---
+++ khoros/utils/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:         khoros.utils.tests
+:Synopsis:       This package includes tests for the khoros library.
+:Created By:     Jeff Shurtliff
+:Last Modified:  Jeff Shurtliff
+:Modified Date:  26 Mar 2020
+"""
