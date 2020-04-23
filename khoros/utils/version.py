bab410257b9faebb5acb5138d028ee22a6668ffd
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2020-04-23 15:17:25 -0600

    Updated the version to 2.1.0 in version.py

--- khoros/utils/version.py
+++ khoros/utils/version.py
@@ -6,14 +6,14 @@
 :Example:           ``__version__ = version.get_full_version()``
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     06 Apr 2020
+:Modified Date:     23 Apr 2020
 """
 
 import warnings
 
 import requests
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 
 def get_full_version():
