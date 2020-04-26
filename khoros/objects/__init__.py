7c52bbe8ff69d6b1062a2b50230d9733c64de577
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2020-04-26 14:12:56 -0600

    Added 'messages' module to __all__ and an import statementt to import it by default when the khoros.objects module is imported

--- khoros/objects/__init__.py
+++ khoros/objects/__init__.py
@@ -6,11 +6,12 @@
 :Example:           ``node_id = base.get_node_id('https://community.khoros.com/t5/Khoros-Blog/bg-p/relnote', 'blog')``
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     19 Apr 2020
+:Modified Date:     26 Apr 2020
 """
 
-__all__ = ['base', 'users']
+__all__ = ['base', 'messages', 'users']
 
 # Import all submodules by default
 from . import base
+from . import messages
 from . import users
