8b38e5173cc0e5fa95009d55dd38aac84df8e400
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2020-05-20 00:09:51 -0600

    Added the khoros.studio package with the base.py module including 6 new functions

---
+++ khoros/studio/base.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            khoros.studio.base
+:Synopsis:          This module handles the base functionality of the studio module.
+:Usage:             ``import khoros.studio.base as studio_base``
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     20 May 2020
+"""
+
+from ..utils import core_utils
+
+
+def sdk_installed():
+    """This function checks to see if the Lithium SDK is installed.
+
+    .. versionadded:: 2.6.0
+
+    :returns: Boolean value indicating whether or not the Lithium SDK is installed
+    """
+    try:
+        output = core_utils.run_cmd('li')
+        is_installed = True if output.returncode == 0 else False
+    except FileNotFoundError:
+        is_installed = False
+    return is_installed
+
+
+def get_sdk_version():
+    """This function identifies the currently installed version of the Lithium SDK.
+
+    .. versionadded:: 2.6.0
+
+    :returns: The SDK version in string format or ``None`` if not installed
+    """
+    version = None
+    if npm_installed():
+        output = core_utils.run_cmd('npm list -g lithium-sdk').stdout
+        output = core_utils.decode_binary(output)
+        if '(empty)' not in output:
+            version = output.split('lithium-sdk@')[1].split(' ')[0]
+    return version
+
+
+def node_installed():
+    """This function checks whether or not Node.js is installed.
+
+    .. versionadded:: 2.6.0
+
+    :returns: Boolean value indicating whether or not Node.js is installed
+    """
+    node_version = get_node_version()
+    return True if node_version else False
+
+
+def get_node_version():
+    """This function identifies and returns the installed Node.js version.
+
+    .. versionadded:: 2.6.0
+
+    :returns: The version as a string or ``None`` if not installed
+    """
+    try:
+        version = core_utils.run_cmd('node -v').stdout.decode('utf-8').strip()[1:]
+    except FileNotFoundError:
+        version = None
+    return version
+
+
+def npm_installed():
+    """This function checks whether or not npm is installed.
+
+    .. versionadded:: 2.6.0
+
+    :returns: Boolean value indicating whether or not npm is installed
+    """
+    npm_version = get_npm_version()
+    return True if npm_version else False
+
+
+def get_npm_version():
+    """This function identifies and returns the installed npm version.
+
+    .. versionadded:: 2.6.0
+
+    :returns: The version as a string or ``None`` if not installed
+    """
+    try:
+        version = core_utils.run_cmd('npm -v').stdout.decode('utf-8').strip()[1:]
+    except FileNotFoundError:
+        version = None
+    return version
