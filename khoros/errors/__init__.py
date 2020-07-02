847be5cf119c176dea5bdd758b42171ca8f7b89b
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2020-07-01 22:32:48 -0600

    Added translations module to __all__ and imported by default

--- khoros/errors/__init__.py
+++ khoros/errors/__init__.py
@@ -6,10 +6,10 @@
 :Example:        ``raise errors.exceptions.MissingAuthDataError``
 :Created By:     Jeff Shurtliff
 :Last Modified:  Jeff Shurtliff
-:Modified Date:  21 Feb 2020
+:Modified Date:  01 Jul 2020
 """
 # Define all modules that will be imported with the "import *" method
-__all__ = ['exceptions', 'handlers']
+__all__ = ['exceptions', 'handlers', 'translations']
 
 # Always import the warnings package and the exceptions module
-from . import exceptions, handlers
+from . import exceptions, handlers, translations
