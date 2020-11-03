c3704a10b696e5aa3895f786caebfa181390bf89
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2020-11-02 20:08:42 -0700

    Changed veresion to 3.1.1

--- khoros/utils/version.py
+++ khoros/utils/version.py
@@ -6,7 +6,7 @@
 :Example:           ``__version__ = version.get_full_version()``
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     28 Oct 2020
+:Modified Date:     02 Nov 2020
 """
 
 import requests
@@ -14,7 +14,7 @@ import requests
 from . import log_utils
 
 # Define special and global variables
-__version__ = "3.1.0"
+__version__ = "3.1.1"
 latest_version_reported = False
 logger = log_utils.initialize_logging(__name__)
 
