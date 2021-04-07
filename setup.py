19b335d6ac82106171da9ef521e8525c4c80846c
Byron Peebles <bpeebles@truthinitiative.org>
2021-04-07 16:13:44 -0400

    Parse version.py to avoid import dependcies for setup.py.
    This is using technique 1 from
    
    https://packaging.python.org/guides/single-sourcing-package-version/
    
    to address this.
    
    Resolves jeffshurtliff/khoros#28

--- setup.py
+++ setup.py
@@ -2,13 +2,28 @@
 """This script is the primary configuration file for the khoros project."""
 
 import setuptools
+import codecs
+import os.path
 
-import khoros.utils.version
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = khoros.utils.version.__version__
+version = get_version("khoros/utils/version.py")
 
 setuptools.setup(
     name="khoros",
