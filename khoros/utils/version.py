4c11e3269923a1e55c522c5d59046f56716b8d23
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2022-01-16 10:52:55 -0700

    Changed the version to 4.5.0

--- khoros/utils/version.py
+++ khoros/utils/version.py
@@ -15,7 +15,7 @@ import urllib.request
 from . import log_utils
 
 # Define special and global variables
-__version__ = "4.5.0b2"
+__version__ = "4.5.0"
 latest_version_reported = False
 logger = log_utils.initialize_logging(__name__)
 
