a3d69685844694877314021720c40cd101a39c72
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2022-09-20 14:00:09 -0600

    Changed spelling of one word in khoros/structurs/__init__.py

--- khoros/structures/__init__.py
+++ khoros/structures/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 :Module:            khoros.structures
-:Synopsis:          This module contains sub-modules that are specific to the various Community structures.
+:Synopsis:          This module contains submodules that are specific to the various Community structures.
 :Usage:             ``from khoros import structures``
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     29 May 2020
+:Modified Date:     20 Sep 2022
 """
 
 __all__ = ['base', 'boards', 'categories', 'communities', 'grouphubs', 'nodes']
