db191686d41ad678965beb9b0a52fec1e07c65ec
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2022-09-29 15:27:16 -0600

    Updated decrypt_control_data.sh to decrypt the categories control data

--- .github/scripts/decrypt_control_data.sh
+++ .github/scripts/decrypt_control_data.sh
@@ -5,9 +5,13 @@
 # Created By:           Jeff Shurtliff
 # Last Modified By:     Jeff Shurtliff
 # Modified Date:        2022-09-29
-# Version:              1.0.0
+# Version:              1.1.0
 
 
 # Decrypt the Communities control data
 gpg --quiet --batch --yes --decrypt --passphrase="$HELPER_DECRYPT_PASSPHRASE" \
 --output "$HOME"/secrets/communities_control_data.json ./.github/encrypted/communities_control_data.json.gpg
+
+# Decrypt the Categories control data
+gpg --quiet --batch --yes --decrypt --passphrase="$HELPER_DECRYPT_PASSPHRASE" \
+--output "$HOME"/secrets/categories_control_data.json ./.github/encrypted/categories_control_data.json.gpg
