d642380ef5aaf9d15fdb7b58163b3ac862c8e27c
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2022-09-29 09:55:05 -0600

    Created the decrypt_control_data.sh script

---
+++ .github/scripts/decrypt_control_data.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Script:               decrypt_control_data.sh
+# Synopsis:             This script decrypts control data JSON files that have been PGP encrypted
+# Created By:           Jeff Shurtliff
+# Last Modified By:     Jeff Shurtliff
+# Modified Date:        2022-09-29
+# Version:              1.0.0
+
+
+# Decrypt the Communities control data
+gpg --quiet --batch --yes --decrypt --passphrase="$HELPER_DECRYPT_PASSPHRASE" \
+--output "$HOME"/secrets/communities_control_data.json ./.github/encrypted/communities_control_data.json.gpg
