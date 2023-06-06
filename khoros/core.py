1745379d96ebe2ea348d3a4ca590fa0db7164de9
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2023-06-06 14:42:20 -0600

    Updated the timestamp in the core.py header block

--- khoros/core.py
+++ khoros/core.py
@@ -6,7 +6,7 @@
 :Example:           ``khoros = Khoros(helper='helper.yml')``
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     02 Nov 2022
+:Modified Date:     06 Jun 2023
 """
 
 import sys
