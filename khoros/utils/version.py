38df3f6023fba0b613d7b48cf9eb5db28cc0e1df
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2023-10-06 13:44:39 -0600

    Changed the version to be 5.4.0rc3

--- khoros/utils/version.py
+++ khoros/utils/version.py
@@ -6,7 +6,7 @@
 :Example:           ``__version__ = version.get_full_version()``
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     11 Sep 2023
+:Modified Date:     06 Oct 2023
 """
 
 import json
@@ -15,7 +15,7 @@ import urllib.request
 from . import log_utils
 
 # Define special and global variables
-__version__ = "5.4.0rc2"
+__version__ = "5.4.0rc3"
 latest_version_reported = False
 logger = log_utils.initialize_logging(__name__)
 
